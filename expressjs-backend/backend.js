86a214c8ad255a0be884fa66a55281f3d6ef2668
Arielle Goodman <goodmanr@calpoly.edu>
2022-04-06 13:24:57 -0700

    s.

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5001;
+const port = 5002;
 
 app.use(express.json());
 
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});     
+});          
