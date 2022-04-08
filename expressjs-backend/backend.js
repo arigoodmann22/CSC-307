51810c2b65f244282535e174ab1b5804d6788997
Arielle Goodman <goodmanr@calpoly.edu>
2022-04-08 13:32:45 -0700

    fix

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const app = express();
-const port = 5002;
+const port = 5004;
+
+const users = { 
+   users_list :
+   [
+      { 
+         id : 'xyz789',
+         name : 'Charlie',
+         job: 'Janitor',
+      },
+      {
+         id : 'abc123', 
+         name: 'Mac',
+         job: 'Bouncer',
+      },
+      {
+         id : 'ppp222', 
+         name: 'Mac',
+         job: 'Professor',
+      }, 
+      {
+         id: 'yat999', 
+         name: 'Dee',
+         job: 'Aspring actress',
+      },
+      {
+         id: 'zap555', 
+         name: 'Dennis', 
+         job: 'Bartender', 
+      }
+   ]
+}
 
 app.use(express.json());
 
@@ -12,6 +43,98 @@ app.get('/users', (req, res) => {
     res.send(users);
 });
 
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+/* implement an additional action to get all users that match a given name and a given job */
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+    if (name != undefined && job != undefined){
+        let result = findUserByNameAndJob(name, job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (name != undefined) {
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+/*implement hard delete operation to remove a particular user by id from the list */
+app.delete('/users/:id', (req, res)=> {
+    const id = req.params.id;
+    const userToDelete = req.body;
+    deleteUser(userToDelete);
+    res.status(200).end();
+    
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-});          
+});   
+
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+function deleteUser(user){
+    const index = users['users_list'].indexOf(user);
+    users['users_list'].splice(index, 1);
+}
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => {
+    return users['users_list'].filter ((user) => user['job'] == job);
+}
+
+function findUserByNameAndJob(name, job){
+    return users['users_list'].find( (user) => user['name'] === name && user['job'] == job);  
+} 
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+       
